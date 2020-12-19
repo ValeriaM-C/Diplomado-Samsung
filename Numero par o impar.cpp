e1d3a4e30f25db608f6e3d6ff401c9458f6dcb97
ValeriaM-C <76227964+ValeriaM-C@users.noreply.github.com>
2020-12-18 19:26:11 -0500

    Add files via upload

---
+++ Numero par o impar.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int num;
+	
+	printf ("Escribe un numero: ");
+	scanf ("%d", &num);
+	
+	if (num%2==0)
+	{
+		
+		printf("Es un numero par");
+		
+	}
+	else
+	{
+		
+		printf ("Es un numero impar");
+		
+	}
+	
+	return 0;
+   
+}
+
+	
+
+
